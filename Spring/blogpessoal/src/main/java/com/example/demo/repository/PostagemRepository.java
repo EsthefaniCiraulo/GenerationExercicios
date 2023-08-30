698d5dcd5d2108b0f13a8a365cd92dae53723485
Esthefani Alves <esthefani.alves@yahoo.com.br>
2023-08-30 14:17:13 -0300

    Exercicios blog pessoal parte 2

--- Spring/blogpessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
+++ Spring/blogpessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
