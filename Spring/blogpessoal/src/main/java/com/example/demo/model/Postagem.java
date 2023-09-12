10437e5ca38b334f950138c797c48cbae74a2839
Esthefani Alves <esthefani.alves@yahoo.com.br>
2023-09-12 19:45:36 -0300

    Exercicio Security

--- Spring/blogpessoal/src/main/java/com/example/demo/model/Postagem.java
+++ Spring/blogpessoal/src/main/java/com/example/demo/model/Postagem.java
@@ -18,64 +18,76 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-    
-    @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@NotBlank(message = "O atributo título é Obrigatório!") 
+
+	@NotBlank(message = "O atributo título é Obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String titulo;
-	
+
 	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
-	
+
 	@UpdateTimestamp
 	private LocalDateTime data;
 
-    @ManyToOne
+	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 
-    public Long getId() {
-        return this.id;
-    }
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return this.id;
+	}
 
-    public String getTitulo() {
-        return this.titulo;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public String getTitulo() {
+		return this.titulo;
+	}
 
-    public String getTexto() {
-        return this.texto;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
+	public String getTexto() {
+		return this.texto;
+	}
 
-    public LocalDateTime getData() {
-        return this.data;
-    }
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
 
-    public void setData(LocalDateTime data) {
-        this.data = data;
-    }
-  
-    public Tema getTema() {
-        return this.tema;
-    }
+	public LocalDateTime getData() {
+		return this.data;
+	}
 
-    public void setTema(Tema tema) {
-        this.tema = tema;
-    }
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Tema getTema() {
+		return this.tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 }
