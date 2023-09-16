53aea8aaedd1e7cd21360cba10ef54a3e6b44d1f
Esthefani Alves <esthefani.alves@yahoo.com.br>
2023-09-15 21:53:42 -0300

    Junit

--- Spring/blogpessoal/src/main/java/com/example/demo/model/Usuario.java
+++ Spring/blogpessoal/src/main/java/com/example/demo/model/Usuario.java
@@ -93,4 +93,16 @@ public class Usuario {
 		this.postagem = postagem;
 	}
 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto){
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() {	}
+	
+	
+
 }
