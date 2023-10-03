afc695256570a504a0ba0e9cf50423a1ddca6c74
Esthefani Alves <esthefani.alves@yahoo.com.br>
2023-10-03 20:55:14 -0300

    blog pessoal rotas - 8

--- FrontEnd/React/blog-pessoal/src/App.tsx
+++ FrontEnd/React/blog-pessoal/src/App.tsx
@@ -1,8 +1,19 @@
-import './App.css'
-import Home from './Home'
-
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 function App() {
-  return <Home></Home>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App
+export default App;
---
+++ FrontEnd/React/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,26 @@
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
+
+
+
+const Home = () => {
+    const navigate = useNavigate()
+    return (
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Home
