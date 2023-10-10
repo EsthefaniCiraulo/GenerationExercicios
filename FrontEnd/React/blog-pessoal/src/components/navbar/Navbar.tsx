0ba9c6d019d8426b18d342c34ac77a5592624bde
Esthefani Alves <esthefani.alves@yahoo.com.br>
2023-10-09 21:56:57 -0300

    Blog Pessoal 23

---
+++ FrontEnd/React/blog-pessoal/src/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
+
+
+function Navbar() {
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
+  
+
+  return (
+    <>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+            <div className='flex gap-4'>
+            <div className='hover:underline'>Postagens</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+              <div className='hover:underline'>Cadastrar tema</div>
+              <div className='hover:underline'>Perfil</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+             
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
